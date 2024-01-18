0e2aa8edfd3ad9df63efef2f1b61030ff612b7d8
Lee Mander <lee.mander@gmail.com>
2024-01-18 14:20:37 +0000

    Add basic game fucntionality

--- javascript-developer/guess-the-word/app.js
+++ javascript-developer/guess-the-word/app.js
@@ -25,13 +25,15 @@ const words = [
   "sticky",
   "gamers",
 ];
+
 const currentWord = words[Math.floor(Math.random() * words.length)];
+
 let tries = 0;
 let mistakes = [];
 
 const wordEl = document.getElementById("word");
 const triesEl = document.getElementById("tries");
-const dotsEl = document.getElementById("dots");
+const dots = document.querySelectorAll(".dots__dot");
 const mistakesEl = document.getElementById("mistakes");
 const inputForm = document.getElementById("input");
 const inputs = [...document.querySelectorAll("input")];
@@ -47,13 +49,46 @@ function scramble(word) {
   return array.join("");
 }
 
+function evaluate(guess) {
+  if (tries < 5) {
+    guess.forEach((letter, index) => {
+      if (letter !== currentWord[index]) {
+        if (mistakes.indexOf(letter) === -1) mistakes.push(letter);
+      }
+    });
+    mistakesEl.innerText = mistakes;
+    if (mistakes.length) {
+      tries++;
+      if (tries < 6) {
+        triesEl.innerText = tries;
+        renderTries();
+      }
+    } else if (!mistakes.length) {
+      alert("You win!");
+    }
+  } else {
+    alert("You lose!");
+  }
+}
+
+function renderTries() {
+  for (let i = 0; i < tries; i++) {
+    dots[i].classList.add("filled");
+  }
+}
+
 wordEl.innerText = scramble(currentWord);
+
 inputs.forEach((input) =>
   input.addEventListener("keyup", (e) => {
     if (e.code === `Key${e.key.toUpperCase()}`) {
       const currentInputIndex = inputs.indexOf(e.target);
       if (currentInputIndex < inputs.length - 1) {
         inputs[currentInputIndex + 1].focus();
+      } else {
+        const inputs = [...inputForm.elements];
+        const guess = inputs.map((input) => input.value);
+        evaluate(guess);
       }
     }
   })
