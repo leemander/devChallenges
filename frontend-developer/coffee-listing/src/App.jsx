dde5869a1453ea6901225f944e3d25143861edde
Lee Mander <lee.mander@gmail.com>
2024-01-22 14:21:29 +0000

    Write CSS

--- frontend-developer/coffee-listing/src/App.jsx
+++ frontend-developer/coffee-listing/src/App.jsx
@@ -4,32 +4,34 @@ import data from "./data.json";
 function App() {
   return (
     <>
-      <main className="collection">
-        <h1>Our Collection</h1>
-        <p className="collection__intro">
-          Introducing our Coffee Collection, a selection of unique coffees from
-          different roast types and origins, expertly roasted in small batches
-          and shipped fresh weekly.
-        </p>
-        <button className="collection__button selected">All Products</button>
-        <button className="collection__button">Available Now</button>
-        <div className="collection__grid">
-          {data.map((coffee, index) => {
-            return (
-              <CoffeeCard
-                available={coffee.available}
-                avgRating={coffee.averageRating}
-                img={coffee.imgSrc}
-                key={index + 1}
-                name={coffee.name}
-                popular={coffee.popular}
-                price={coffee.price}
-                ratings={coffee.ratings}
-              />
-            );
-          })}
-        </div>
-      </main>
+      <div className="container">
+        <main className="collection">
+          <h1>Our Collection</h1>
+          <p className="collection__intro">
+            Introducing our Coffee Collection, a selection of unique coffees
+            from different roast types and origins, expertly roasted in small
+            batches and shipped fresh weekly.
+          </p>
+          <button className="collection__button selected">All Products</button>
+          <button className="collection__button">Available Now</button>
+          <div className="collection__grid">
+            {data.map((coffee, index) => {
+              return (
+                <CoffeeCard
+                  available={coffee.available}
+                  avgRating={coffee.averageRating}
+                  img={coffee.imgSrc}
+                  key={index + 1}
+                  name={coffee.name}
+                  popular={coffee.popular}
+                  price={coffee.price}
+                  ratings={coffee.ratings}
+                />
+              );
+            })}
+          </div>
+        </main>
+      </div>
     </>
   );
 }
--- frontend-developer/coffee-listing/src/CoffeeCard.jsx
+++ frontend-developer/coffee-listing/src/CoffeeCard.jsx
@@ -1,6 +1,6 @@
 export default function CoffeeCard(props) {
   return (
-    <article className={`collection__card ${props.popular && "popular"}`}>
+    <article className={`collection__card ${props.popular ? "popular" : ""}`}>
       <img src={props.img} alt={props.name} className="card__img" />
       <div className="justify-between">
         <h2>{props.name}</h2>
@@ -15,12 +15,12 @@ export default function CoffeeCard(props) {
             alt="Star rating"
           />
           {props.ratings > 0 ? (
-            <span>
+            <span className="card__rating">
               {props.avgRating}
-              <span className="card__ratings"> ({props.ratings})</span>
+              <span className="card__ratings"> ({props.ratings} votes)</span>
             </span>
           ) : (
-            <span className="card__ratings">No ratings</span>
+            <span className="card__rating card__ratings">No ratings</span>
           )}
         </div>
         {!props.available && <span className="card__sold-out">Sold out</span>}
