98993d6d03bfd8bf76e024184ef3c9579587e0ad
Lee Mander <lee.mander@gmail.com>
2024-01-22 15:38:17 +0000

    Attempt image fix

--- frontend-developer/coffee-listing/src/CoffeeCard.jsx
+++ frontend-developer/coffee-listing/src/CoffeeCard.jsx
@@ -9,9 +9,7 @@ export default function CoffeeCard(props) {
       <div className="justify-between">
         <div className="flex">
           <img
-            src={
-              props.ratings > 0 ? "./images/Star_fill.svg" : "./images/Star.svg"
-            }
+            src={props.ratings > 0 ? "./Star_fill.svg" : "./Star.svg"}
             alt="Star rating"
           />
           {props.ratings > 0 ? (
