dfc67901846aec7b5af43d77792d6fca89161677
Lee Mander <lee.mander@gmail.com>
2024-01-22 15:09:58 +0000

    Update config

--- frontend-developer/coffee-listing/vite.config.js
+++ frontend-developer/coffee-listing/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend-developer/coffee-listing/",
+  base: "/devChallenges/frontend-developer/coffee-listing/",
 });
