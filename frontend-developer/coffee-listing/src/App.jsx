587c352594d8eda8f751411ff04b95bbb2a68679
Lee Mander <lee.mander@gmail.com>
2024-01-22 11:03:58 +0000

    Write static JSX

--- frontend-developer/coffee-listing/src/App.jsx
+++ frontend-developer/coffee-listing/src/App.jsx
@@ -1,5 +1,19 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="collection">
+        <h1>Our Collection</h1>
+        <p className="collection__intro">
+          Introducing our Coffee Collection, a selection of unique coffees from
+          different roast types and origins, expertly roasted in small batches
+          and shipped fresh weekly.
+        </p>
+        <button className="collection__button selected">All Products</button>
+        <button className="collection__button">Available Now</button>
+        <div className="collection__grid"></div>
+      </main>
+    </>
+  );
 }
 
 export default App;
