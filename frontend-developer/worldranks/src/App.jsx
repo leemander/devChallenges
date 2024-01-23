f9e3f7bab27e6863d3ce9396f8d2898453682242
Lee Mander <lee.mander@gmail.com>
2024-01-23 16:05:28 +0000

    Add assets and set up CSS

--- frontend-developer/worldranks/src/App.jsx
+++ frontend-developer/worldranks/src/App.jsx
@@ -2,7 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus,
+        dolor, ullam amet repellat in eligendi harum cum inventore ab vero vitae
+        libero officiis eos error quaerat quia! Provident, deleniti voluptas!
+      </p>
+    </>
+  );
 }
 
 export default App;
