3a01d63ac3a72166f489f2b53c2206f620ac5325
Lee Mander <lee.mander@gmail.com>
2024-01-29 14:44:04 +0000

    Refactor into pages

--- frontend-developer/worldranks/src/TableRow.jsx
+++ frontend-developer/worldranks/src/TableRow.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function TableRow(props) {
   return (
     <tr>
       <td>
         <img src={props.img} alt={props.alt} />
       </td>
-      <td>{props.name}</td>
+      <td>
+        <Link to={`/country/${props.name.toLowerCase()}`}>{props.name}</Link>
+      </td>
       <td>{props.pop}</td>
       <td>{props.area}</td>
     </tr>
